932c65ad166a9a38fa37e2929a18bbacb5c1cd0c
Taha_Adeel <cs20btech11052@iith.ac.in>
2021-06-12 01:02:55 +0530

    Moved game logic from main.cpp to Game class.

---
+++ src/game.cpp
@@ -0,0 +1,55 @@
+#include "game.h"
+
+Game::Game(): m_window(sf::VideoMode(720, 480), "Edge of the Abyss", 
+							sf::Style::Titlebar | sf::Style::Close){
+	m_window.setFramerateLimit(60);
+}
+
+// Runs the main loop
+void Game::run(){
+	sf::Clock timer;
+	auto lastTime = sf::Time::Zero;
+
+	while(m_window.isOpen()){
+		auto time = timer.getElapsedTime();
+		lastTime = time;
+		timer.restart();
+
+		//elapsedTime is used to update our physics properly, so that gameplay is independent of FPS
+		auto elapsedTime = time - lastTime;
+
+
+		m_window.clear(sf::Color::Blue);
+		//render stuff
+        m_window.display();
+
+		handleEvent();
+	}
+}
+
+// Handles window events, called every frame
+void Game::handleEvent()
+{
+    sf::Event e;
+
+    while (m_window.pollEvent(e)) {
+        switch (e.type) {
+            case sf::Event::Closed:
+                m_window.close();
+                break;
+			case sf::Event::KeyPressed:
+			if(e.key.code == sf::Keyboard::Escape)
+				m_window.close();
+
+            default:
+                break;
+        }
+    }
+	if(m_shouldExit)
+		m_window.close();
+}
+
+
+void Game::exitGame(){
+	m_shouldExit = true;
+}
