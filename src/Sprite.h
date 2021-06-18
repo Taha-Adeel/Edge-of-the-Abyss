2fdda9708169ffc4130ad4d53dd2ed783fd10f86
Sushma-AI1103 <sp067611@gmail.com>
2021-06-18 17:33:25 +0530

    add sprite class

---
+++ src/Sprite.h
@@ -0,0 +1,36 @@
+#ifndef SPRITE_H
+#define SPRITE_H
+
+#include <string>
+#include <SFML/Graphics.hpp>
+
+#include "../ResourceManager/ResourceFactory.h"
+
+/**
+ * @brief class Sprite will load the sprite 
+ * 
+ */
+
+class Sprite: public sf::Sprite{
+public:
+	//public constructor 
+	
+	Sprite(std::string name);
+	Sprite(std::string name, int index);
+	Sprite(std::string name, sf::IntRect rect);
+
+	//public functions
+	//Define similar setTexture functions as constructors
+
+private:
+	//private members
+	const sf::Texture& m_spritesheet;
+
+	//private constants
+	int m_tilewidth = 42;
+	int m_tileheight = 42;
+	int m_tilespacing = 2;
+};
+
+
+#endif
