08820648fad60c751696b97627d8d172a513e24f
Vamsi_Preetham <cs20btech11058@iith.ac.in>
2021-07-11 09:15:32 +0530

    Map 1 created

---
+++ maps/map5tile5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.0" name="map5tile5" tilewidth="40" tileheight="40" spacing="2" tilecount="6" columns="6">
+ <image source="../../Downloads/smallBlocks.png" width="264" height="44"/>
+</tileset>
