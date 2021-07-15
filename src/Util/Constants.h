1784f7565c0c273f37bbf0b48746bef58ac04d07
Sushma-AI1103 <sp067611@gmail.com>
2021-07-15 20:12:27 +0530

    constant.h updated

--- src/Util/Constants.h
+++ src/Util/Constants.h
@@ -14,19 +14,19 @@ namespace CONSTANTS{
 
 	constexpr float SPAWNPOINT_X = 200.f;
 	constexpr float SPAWNPOINT_Y = GROUNDHEIGHT;
-	constexpr float PLAYER_SPEED_X = 0.f;
-	constexpr float PLAYER_SPEED_Y = 0.f;
+	constexpr float PLAYER_SPEED_X = 100.f;
+	constexpr float PLAYER_SPEED_Y = -100.f;
 	constexpr float PLAYER_JUMP_BOOST = -700.f;
 
 	constexpr int TILE_WIDTH = 42;
 	constexpr int TILE_HEIGHT = 42;
 	constexpr int TILE_SPACING = 2;
 
-	constexpr int GROUND_HEIGHT =  0 ;//3 * TILE_WIDTH;
-    constexpr int CAMERA_OFFSET_Y = -385;
-    constexpr int CAMERA_OFFSET_X = 0 ;//-400;
-    constexpr int CAMERA_BOX_TOP_Y = 250;
-    constexpr int CAMERA_BOX_BOTTOM_Y = 450;
+	
+    constexpr int CAMERA_OFFSET_Y_TOP = 120;
+    constexpr int CAMERA_OFFSET_X_LEFT = 300;
+    constexpr int CAMERA_OFFSET_Y_BOTTOM = 400;
+    constexpr int CAMERA_OFFSET_GROUND_Y = 450;
 
 }
 
