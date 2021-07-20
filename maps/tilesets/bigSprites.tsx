2329e5815ccde905ae10c0099725e40e1b442db1
Vamsi_Preetham <cs20btech11058@iith.ac.in>
2021-07-20 11:56:41 +0530

    Game map created

---
+++ maps/tilesets/bigSprites.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.1" name="bigSprites" tilewidth="42" tileheight="42" spacing="2" tilecount="6" columns="3">
+ <image source="../assets/bigSprites.png" width="172" height="86"/>
+</tileset>
