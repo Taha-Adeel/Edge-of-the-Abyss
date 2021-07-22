8c517b96317ec6d58c61ea9d9c0c7e5d56e41c55
prashanthsriram-s <prashanthsriram10@gmail.com>
2021-07-22 20:02:28 +0530

    Added changeGameMode

--- src/States/PlayingState.cpp
+++ src/States/PlayingState.cpp
@@ -1,5 +1,5 @@
 #include "PlayingState.h"
-
+#include <thread>
 /**
  * @brief Construct a new Playing State object
  * 
@@ -11,8 +11,40 @@ PlayingState::PlayingState(Game& pGame):
 	m_player(std::make_unique<NormalPlayer>(*this)),
 	m_level("checkmap", *this),
 	m_camera(*this),
-	m_scoreKeeper(*this)
+	m_scoreKeeper(*this),
+	m_gameMode(GAMEMODE::NORMAL)
 {
+}
+
+/**
+ * @brief makes the scorekeeper update the highscore at the end of the game if needed.
+ * 
+ */
+void PlayingState::displayGameEnd()
+{
+	m_scoreKeeper.updateHighScore();
+}
+
+/**
+ * @brief Changes the current game mode to required mode
+ * 		  It takes a GAMEMODE as a parameter instead of merely switching between NORMAL and PLAYER
+ * 		  based on current GAMEMODE so that when further game modes are used, it is easy to extend
+ * @param gameMode of type GAMEMODE enum class, NORMAL or PLANE
+ */
+void PlayingState::changeGameMode(GAMEMODE gameMode)
+{
+	if(m_gameMode == gameMode)return; // No need to change
+	std::this_thread::sleep_for(std::chrono::milliseconds(500)); // Gives a small pause for new map
+	m_gameMode = gameMode;
+	m_camera.reset();
+	if(gameMode == GAMEMODE::NORMAL)
+	{
+		m_player = std::make_unique<NormalPlayer>(*this);
+	}
+	else if(gameMode == GAMEMODE::PLANE)
+	{
+		m_player = std::make_unique<PlanePlayer>(*this);
+	}
 }
 /**
  * @brief Delegates event handeling to player, camera, etc
@@ -73,8 +105,4 @@ Camera& PlayingState::getCamera() {
  */
 const Level& PlayingState::getCurrentLevel() const {
 	return m_level;
-}
-void PlayingState::displayGameEnd()
-{
-	m_scoreKeeper.updateHighScore();
 }
