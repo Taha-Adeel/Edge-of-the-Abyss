b9a3eaa676217477b926c2a909bb6b15986c9b74
Vamsi_Preetham <cs20btech11058@iith.ac.in>
2021-07-22 08:41:41 +0530

    Game map corrected

--- maps/tilesets/portal.tsx
+++ maps/tilesets/portal.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.1" name="portal" tilewidth="42" tileheight="42" spacing="2" tilecount="4" columns="2">
- <image source="../assets/portal.png" width="92" height="87"/>
+<tileset version="1.5" tiledversion="1.7.1" name="portal" tilewidth="42" tileheight="42" tilecount="4" columns="2">
+ <image source="../../assets/portal.png" width="84" height="84"/>
 </tileset>
